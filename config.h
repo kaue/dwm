aa51deb0c635f1d6a1105748dc915d5babbe2e10
wtl <wtl144000@gmail.com>
2020-05-22 22:38:38 +0300

    swallow X windows from the terminal

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 10;        /* 0 means no bar */
 static const int topbar             = 10;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -30,6 +31,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	/* { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 }, */
+	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
