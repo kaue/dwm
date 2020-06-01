147b392ca36e80fee8f237d0a5a4620c9b03e2c7
Kauê Gimenes <kaue.gimenes@gmail.com>
2020-05-31 21:35:50 -0300

    +Fira Code

--- config.h
+++ config.h
@@ -61,7 +61,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-modi", "run", "-show", "run" };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-font", "Fira Code", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
